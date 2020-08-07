448967295a90e4201b06f188bcce3d3dc6feb654
Junyoung Clare Jang <jjc9310@gmail.com>
2020-08-07 04:01:37 +0000

    Move test codes into the test component

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,3 @@ module Main where
 import Parser
 
 main = print "Hello"
-
-test = do
-  testReaXML "test-data/reaxml0.txt"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,3 +1,25 @@
 module Main where
 
-main = putStrLn "No test has been implemented!"
+import Parser.ReaCSS
+import Parser.ReaXML
+import Text.Megaparsec
+import Prettyprinter (Pretty, pretty)
+import Prettyprinter.Util (putDocW)
+
+main :: IO ()
+main = do
+  testReaXML "test/fixture/reaxml0.txt"
+  testReaCSS "test/fixture/reacss0.txt"
+
+testReaCSS :: FilePath -> IO ()
+testReaCSS = testParserWithFile reaCSS
+
+testReaXML :: FilePath -> IO ()
+testReaXML = testParserWithFile reaXML
+
+testParserWithFile :: (Pretty a) => Parser a -> FilePath -> IO ()
+testParserWithFile p fp = do
+  str <- readFile fp
+  case parse p fp str of
+    Left err -> print err
+    Right v -> putDocW 80 $ pretty v
