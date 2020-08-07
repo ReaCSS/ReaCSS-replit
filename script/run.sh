f4d4284aa7ff78c693d294cc2c2c72ecc16eb456
Junyoung Clare Jang <jjc9310@gmail.com>
2020-08-07 02:50:50 +0000

    Update run.sh to  work with repl.run page

--- script/run.sh
+++ script/run.sh
@@ -1,13 +1,33 @@
 #/usr/bin/env bash
 
-if [ ! -d "$(pwd)/.cabal-sandbox" ]; then
+is_ide_run () {
+  [ "$(basename "$(pwd)")" = "ReaCSS" ]
+}
+
+on_ide_run () {
+  if [ ! -d "$(pwd)/.cabal-sandbox" ]; then
+    cabal v1-sandbox init
+  fi
+
+  cabal v1-install --offline --only-dependencies --dry-run 2>&1 1> /dev/null
+  if [ $? -ne 0 ]; then
+    cabal v1-update
+    cabal v1-install --only-dependencies -j1
+  fi
+
+  cabal v1-run exe:ReaCSS
+}
+
+on_run_only_tab () {
+  rm -rf .cabal-sandbox
   cabal v1-sandbox init
-fi
-
-cabal v1-install --offline --only-dependencies --dry-run 2>&1 1> /dev/null
-if [ $? -ne 0 ]; then
   cabal v1-update
-  cabal v1-install --only-dependencies
-fi
+  cabal v1-install --only-dependencies -j1
+  cabal v1-run exe:ReaCSS
+}
 
-cabal v1-run exe:ReaCSS
+if is_ide_run; then
+  time on_ide_run
+else
+  time on_run_only_tab
+fi
